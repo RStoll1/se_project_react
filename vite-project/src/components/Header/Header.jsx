1192c7b29c28777453887c47d64feb516e89c68c
morti3l <pushthatboard@gmail.com>
2025-10-06 17:25:49 -0500

    Add functionality to modals. Add dynamic weather cards to reflect current weather. Add filtering of cards for current weather. Complete jsx and markup for app.

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,18 +1,31 @@
-import "./Header.css"
+import "./Header.css";
 import logo from "../../assets/Logo.svg";
 import avatar from "../../assets/AvatarLogo.svg";
 
-
-function Header() {
-    return <header className="header">
-        <img src={logo} alt="Header Logo" className="header__logo" />
-        <p className="header__date-location">Date, Location</p>
-        <button className="header__add-clothes-btn">+ Add clothes</button>
-        <div className="header__user-container">
-            <p className="header__username">Terrance Tegegne</p>
-            <img src={avatar} alt="Header Avatar" className="header__avatar" />
-        </div>
+function Header({ handleAddClick, weatherData }) {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+  return (
+    <header className="header">
+      <img src={logo} alt="Header Logo" className="header__logo" />
+      <p className="header__date-location">
+        {currentDate}, {weatherData.city}
+      </p>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        + Add clothes
+      </button>
+      <div className="header__user-container">
+        <p className="header__username">Terrance Tegegne</p>
+        <img src={avatar} alt="Header Avatar" className="header__avatar" />
+      </div>
     </header>
+  );
 }
 
-export default Header
+export default Header;
--- vite-project/src/utils/constants.js
+++ vite-project/src/utils/constants.js
@@ -1,3 +1,76 @@
+export const weatherOptions = [
+    {
+        day: true,
+        condition: "clear",
+        url: new URL("../assets/day/clear.svg", import.meta.url).href,
+    },
+    {
+        day: true,
+        condition: "clouds",
+        url: new URL("../assets/day/cloudy.svg", import.meta.url).href,
+    },
+    {
+        day: true,
+        condition: "fog",
+        url: new URL("../assets/day/fogday.svg", import.meta.url).href,
+    },
+    {
+        day: true,
+        condition: "rain",
+        url: new URL("../assets/day/rainday.svg", import.meta.url).href,
+    },
+    {
+        day: true,
+        condition: "snow",
+        url: new URL("../assets/day/snowday.svg", import.meta.url).href,
+    },
+    {
+        day: true,
+        condition: "thunderstorm",
+        url: new URL("../assets/day/stormday.svg", import.meta.url).href,
+    },
+    {
+        day: false,
+        condition: "clear",
+        url: new URL("../assets/night/clearnight.svg", import.meta.url).href,
+    },
+    {
+        day: false,
+        condition: "clouds",
+        url: new URL("../assets/night/cloudynight.svg", import.meta.url).href,
+    },
+    {
+        day: false,
+        condition: "fog",
+        url: new URL("../assets/night/fognight.svg", import.meta.url).href,
+    },
+    {
+        day: false,
+        condition: "rain",
+        url: new URL("../assets/night/rainnight.svg", import.meta.url).href,
+    },
+    {
+        day: false,
+        condition: "snow",
+        url: new URL("../assets/night/snownight.svg", import.meta.url).href,
+    },
+    {
+        day: false,
+        condition: "thunderstorm",
+        url: new URL("../assets/night/stormnight.svg", import.meta.url).href,
+    },
+];
+
+export const defaultWeatherOptions = {
+    day: {
+        url: new URL("../assets/day/default.svg", import.meta.url).href,
+    },
+    day: {
+        url: new URL("../assets/night/default.svg", import.meta.url).href,
+    },
+};
+
+
 const defaultClothingItems = [
     {
         _id: 1,
@@ -112,5 +185,13 @@ const defaultClothingItems = [
             "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/T-Shirt.png?etag=44ed1963c44ab19cd2f5011522c5fc09",
     },
 ];
+
+
+export const coordinates = {
+    latitude: 36.162663,
+    longitude: -86.781601,
+}
+
+export const APIkey = "51829f627a37d9058fac72440de050e5";
 
 export { defaultClothingItems };
