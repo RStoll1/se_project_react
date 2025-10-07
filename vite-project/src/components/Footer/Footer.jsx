f953fe041c8a2036793fdeacee188a77cd13fdcc
morti3l <pushthatboard@gmail.com>
2025-10-06 23:26:03 -0500

    Fix vite.

---
+++ vite-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__author"> Developed by Ryan Stolinski </p>
+      <p className="footer__date"> 2025 </p>
+    </footer>
+  );
+}
+
+export default Footer;
