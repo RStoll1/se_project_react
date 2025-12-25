af1c71b84aa8ea7254060c4682cde4cb41bd5bdb
morti3l <pushthatboard@gmail.com>
2025-12-24 21:43:05 -0600

    Add baseURL.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production"
-    ? "https://api.whattowearrs.crabdance.com"
+    ? "http://api.whattowearrs.crabdance.com"
     : "http://localhost:3001";
 
 export const jsonHeaders = {
